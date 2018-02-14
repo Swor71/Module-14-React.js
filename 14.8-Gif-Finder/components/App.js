d19e705947821d8e9418bf6e0e5ac59f7be258ed
Marcin Swornowski <swor71@gmail.com>
2018-02-14 15:02:04 +0100

    Add Promises to App.js

--- 14.8-Gif-Finder/components/App.js
+++ 14.8-Gif-Finder/components/App.js
@@ -10,38 +10,48 @@ App = React.createClass({
         };
     },
 
+    getGif: function(searchingText) {
+        return new Promise(
+            (resolve, reject) => {
+                var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', url);
+                xhr.onload = () => {
+                    if (xhr.status === 200) {
+                        var data = JSON.parse(xhr.responseText).data;
+                        var gif = {
+                            url: data.fixed_width_downsampled_url,
+                            sourceUrl: data.url,
+                        };
+                        resolve(gif);
+                    } else {
+                        reject(console.log(error))
+                    }
+                };
+                xhr.onerror = () => {
+                    reject(new Error('Failed to load the gif!'))
+                };
+                
+                xhr.send();
+            }
+        );
+    },
+
     handleSearch: function (searchingText) {
         this.setState({
             loading: true
         });
-        this.getGif(searchingText, function(gif) {
+        this.getGif(searchingText).then( (gif) => {
             this.setState({
                 loading: false,
                 gif: gif,
                 searchingText: searchingText
-            });
-        }.bind(this));
-    },
-
-    getGif: function(searchingText, callback) {
-        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                var data = JSON.parse(xhr.responseText).data;
-                var gif = {
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url,
-                };
-                callback(gif);
-            }
-        };
-        xhr.send();
+            })
+        })
+        .catch((error) => {console.log(error)})
     },
 
     render: function() {
-
         var styles = {
             margin: '0 auto',
             textAlign: 'center',
